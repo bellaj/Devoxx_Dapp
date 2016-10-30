fb488a226b16143c2659cd217c6f43aff9ba6451
badr bellaj <bellaj.badr@gmail.com>
2016-10-30 23:43:17 +0100

    improvement

--- functions.js
+++ functions.js
@@ -12,63 +12,49 @@
       web3.eth.getAccounts(function(err, accounts){if(!err && accounts.length > 0) account_number=accounts.length;      });
 
 	  
-	  
-var abiArray =[{"constant":false,"inputs":[{"name":"name_","type":"string"}],"name":"register_attendee","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"speaker_number","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"name_","type":"string"},{"name":"talk","type":"string"}],"name":"register_speaker","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"attendee_number","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"address_speaker","type":"address"},{"name":"evaluation","type":"uint256"}],"name":"evaluate_speaker","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"reward_best_speaker","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"payable":true,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"id","type":"uint256"}],"name":"attendee_added","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"i","type":"uint256"}],"name":"thankyou","type":"event"}]
+	  /***********************************************Contract instantiation *********************************************************/	  
 
+var abiArray =[{"constant":false,"inputs":[{"name":"name_","type":"string"}],"name":"register_attendee","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"speaker_number","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"name_","type":"string"},{"name":"talk","type":"string"}],"name":"register_speaker","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"attendee_number","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"address_speaker","type":"address"},{"name":"evaluation","type":"uint256"}],"name":"evaluate_speaker","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"reward_best_speaker","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"payable":true,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"id","type":"uint256"}],"name":"attendee_added","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"i","type":"uint256"}],"name":"thankyou","type":"event"}];
 
- ;
-
-
-	  
-/***********************************************Contract instantiation *********************************************************/	  
 var MyContract = web3.eth.contract(abiArray);
-var contractInstance = MyContract.at("0xabf40faa5b08bed95023e974c6d4a9eb50ea85f8");// Instantiate from an existing address, PLEASE BE CAREFUL TO SPACES
-/***********************************************Contract instantiation *********************************************************/	  
-
+var contract_address="0xabf40faa5b08bed95023e974c6d4a9eb50ea85f8";
+var contractInstance = MyContract.at(contract_address);//   Instantiate from an existing address, PLEASE BE CAREFUL TO SPACES
  
-function get_accounts_()
+ /*********************************************** *********************************************************/	  
+function get_accounts_() //get the number of the existent accounts 
 {  
       web3.eth.getAccounts(function(err, accounts){if(!err && accounts.length > 0) account_number=accounts.length;   document.getElementById("account_number").innerHTML ='You have :'+ account_number+' accounts choose ';     });
       
 }
 
-function register_attendee(){
-	 var e = document.getElementById("account");
-	 var name=document.getElementById("name_attendee").value;
-	 var acc =e.options[e.selectedIndex].value;
-	 var account_=web3.eth.accounts[acc];
-	 var payment = document.getElementById("attendee_pay").value;
+  /*********************************************** Register attendees *********************************************************/	  
 
-	// document.getElementById("attendee_message").innerHTML ='Loading ';  
-	 
-	 var transactionObject = {from: account_,value: web3.toWei(payment, "ether"),gas: 500000}; // with donation of 1Ether //cost of tx is 21000 wei //web3.toWei Converts an ethereum unit into wei.
+function register_attendee(){ 
+	
+ 
+	 var transactionObject = {from: account_,value: web3.toWei(payment, "ether"),gas: 500000}; // the gasprice is not indicated is set to default;
+     //One transaction costs at least 21000 gas  //web3.toWei Converts an ethereum unit into wei.
+	 //gas = (optional, default: To-Be-Determined) The amount of gas to use for the transaction (unused gas is refunded).
+	 //Default : Frontier was launched with a default gas price of 0.05e12 wei=50Gwei. In Homestead, the default was reduced to 0.02e12 wei =20Gwei.
+	 //in mainnet 1gas=20gwei.
 
 	 var name= document.getElementById("name_attendee");
-	 
-/*	var filter =contractInstance.attendee_added({},function(error, result) {
-          if (!error) {
-		  document.getElementById("attendee_message").innerHTML ='Result :'+ result.args.msg; console.log(result.args.msg);  } else document.getElementById("attendee_message").innerHTML ='Error :'+ error; 
-
-});	 
-
-filter.watch(function(error, result){
-  if (!error)
-    console.log(result);
-});
-*/
+ 
    var TxHash = contractInstance["register_attendee"].sendTransaction(name, {from: account_}, function(err, address) {
     // console.log(address);
    });
-   
-
-	
+   	
 }
-   var contractEvent = contractInstance["attendee_added"];
-    contractEvent({}).watch(function(error, result){
+ /***********************************************Listening to upcoming Events (successful registration) *********************************************************/	  
+
+   var FilterEvent = contractInstance["attendee_added"]; //filter
+    FilterEvent({}).watch(function(error, result){
   if (!error)
     console.log(result.args.id);document.getElementById("attendee_message").innerHTML ='your ID is '+ result.args.id;
 });
-function check_balance(){
+
+ /***********************************************check the balance of an account *********************************************************/	  
+function check_balance(){ //get the number of the existent accounts 
 		 var e = document.getElementById("account");
 	     var name=document.getElementById("name_attendee").value;
 	     var acc =e.options[e.selectedIndex].value;
@@ -76,4 +62,23 @@ function check_balance(){
 		 var balance =   web3.fromWei(web3.eth.getBalance(account_));
 		 document.getElementById("account_balance").innerHTML ='Balance :'+ balance+' Ether';  
 
+}
+  /*********************************************** Register speaker *********************************************************/	  
+
+function register_speaker(){ 
+	 var e = document.getElementById("account");
+	 var name=document.getElementById("name_speaker").value;
+	 var acc =e.options[e.selectedIndex].value;
+	 var account_=web3.eth.accounts[acc];
+	 var payment = document.getElementById("speaker_pay").value;
+ 
+	 var transactionObject = {from: account_,value: web3.toWei(payment, "ether"),gas: 500000}; // cost of tx is 21000 wei //web3.toWei Converts an ethereum unit into wei.
+
+	 var name= document.getElementById("name_speaker");
+	 var keynote= document.getElementById("keynote");
+ 
+   var TxHash = contractInstance["register_speaker"].sendTransaction(name, {from: account_}, function(err, address) {
+    // console.log(address);
+   });
+   	
 }
